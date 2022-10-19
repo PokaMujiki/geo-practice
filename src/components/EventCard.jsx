2471501a29ce435a8456f1563450ce12f3fa02b4
Alexandr Baranov <baranovau216@gmail.com>
2022-10-19 22:11:45 +0700

    reforamt event info in event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -5,9 +5,11 @@ export const EventCard = ({geoEvent}) => {
   const time = format(new Date(geoEvent.time), "HH:mm:ss");
   return (
     <div className="event_card">
-      <p>{date}   {time}</p>
-      <p>Type: {geoEvent.type}</p>
-      <p>Magnitude: {geoEvent.magnitude}({geoEvent.magnitudeType})</p>
+      <p>{date} {time}</p>
+      <p>
+        {geoEvent.type[0].toUpperCase() +
+          geoEvent.type.substring(1)} magnitude: {geoEvent.magnitude}({geoEvent.magnitudeType})
+      </p>
       <p>Lng: {geoEvent.longitude} Lat: {geoEvent.latitude}</p>
       <p>Network code: {geoEvent.networkCode}</p>
       <p>Depth: {geoEvent.depth}({geoEvent.depthType}, uncertainty: {geoEvent.depthUncertainty})</p>
